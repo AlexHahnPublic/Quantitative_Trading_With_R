19fd5cca49554506ac3b7e2f104adbe6ffd8e917
AlexHahnPublic <afh53.cornell.edu>
2016-11-30 01:52:40 -0500

    finished chapter 2

---
+++ Chapter2_ToolsOfTheTrade/importTickersAdjClose.py
@@ -0,0 +1,48 @@
+#==========================================================
+#=============== importTickersAdjClose.py =================
+#==========================================================
+
+# Purpose
+#----------------------------------------------------------
+# Given dates and ticker names as inputs, create an Adj_close.csv output file
+# of each tickers yahoo finance adj close prices for that interval
+
+#TODO list:
+#----------------------------------------------------------
+# - Error handling of ticker name input
+#   - All symbols may not exist for the interval
+#   -
+
+import datetime as dt
+import pandas as pd
+import pandas_datareader.data as web
+
+if __name__ == "__main__":
+    # Collect the start date
+    start_year = raw_input("Please enter start year: ")
+    start_month = raw_input("Please enter start month (numeric): ")
+    start_day = raw_input("Please enter starting day of the month: ")
+
+    # Collect end date
+    end_year = raw_input("Please enter end year: ")
+    end_month = raw_input("Please enter end month (numeric): ")
+    end_day = raw_input("Please enter ending day of the month: ")
+
+    # Construct start_date and end_date datetime object
+    start_date = dt.datetime(int(start_year), int(start_month), int(start_day))
+    end_date = dt.datetime(int(end_year), int(end_month), int(end_day))
+
+    # Gather the tickers list
+    raw_ticker_lst = raw_input("Please enter the ticker names with a space in \
+between: ")
+
+    # Split into a list
+    ticker_list = raw_ticker_lst.split()
+
+    # Retrieve Yahoo Finance data
+    yahoo_finance_data = web.DataReader(ticker_list, 'yahoo', start_date,
+            end_date)
+
+    # Output the Adj_Close .csv containing all tickers
+    yahoo_finance_data["Adj Close"].to_csv("Adj_close.csv")
+
---
+++ Chapter2_ToolsOfTheTrade/pairwiseCorrelationBetweenTickers.r
@@ -0,0 +1,101 @@
+###########################################################
+########## pairwiseCorrelationBetween Tickers.r ###########
+###########################################################
+
+# Purpose
+# ---------------------------------------------------------
+# Compute and plot the correlations matrix of the returns of a given list of
+# symbols
+
+filter_symbols <- function(symbols) {
+    # Name: filter_symbols
+    # Purpose: Convert to upper case if not, remove any non valid symbols
+    # Input: symbols = vector of stock tickers
+    # Output: filtered_symbols = filtered symbols
+
+    # Convert symbols to uppercase
+    symbols <- toupper(symbols)
+
+    # Validate the symbol names (must be between 2 and 4 chars, no numbers)
+    valid <- regexpr("^[A-Z]{2,4}$", symbols)
+
+    # Return only the valid ones
+    return(sort(symbols[valid == 1]))
+}
+
+extract_prices <- function(filtered_symbols, file_path) {
+  # Name: extract_prices
+  # Purpose: Read price data from a specified file
+  # Inputs: filtered_symbols = vector of symbols,
+  #         file_path = location of price data
+  # Output: prices = data.frame of prices per symbol
+
+  # Read in the .csv price file
+  all_prices <- read.csv(file = file_path, header=TRUE, stringsAsFactors = FALSE)
+
+  # Remove the original Date column
+  all_prices$Date <- NULL
+
+  # Extract only the relevant data columns
+  valid_columns <- colnames(all_prices) %in% filtered_symbols
+
+  return(all_prices[, valid_columns])
+}
+
+filter_prices <- function(prices) {
+  # Name: filter_prices
+  # Purpose: Idetify rows with missing values
+  # Inputs: Prices = data.frame of prices
+  # Output: missing_rows = vector of indexes where data is missing out of one
+  #         or more columns
+
+  # Returns a boolean vector of dense or not fullt dense rows
+  valid_rows <- complete.cases(prices)
+
+  # Identify the index of the missing rows
+  missing_rows <- which(valid_rows == FALSE)
+
+  return(missing_rows)
+}
+
+compute_pairwise_correlations <- function(prices) {
+  # Name: compute_pairwise_correlations
+  # Purpose: Calculates pairwise correlations of returns and plots the pairwise
+  #          relationshps
+  # Inputs: prices = data.frame of prices
+  # Output: correlation_matrix = A correlation matrix
+
+  # Convert prices to returns
+  returns <- apply(prices, 2, function(x) diff(log(x)))
+
+  # Plot all pairwise relationships
+  pairs(returns, main = "Pairwise return scatter plot")
+
+  # Compute the pairwise correlations
+  correlation_matrix <- cor(returns, use = "complete.obs")
+
+  return(correlation_matrix)
+}
+
+#Execution
+#--------------------------------------------------------------------
+# Now we can leverage our helper functions to run a full analysis
+
+# Stock tickers entered by user (note some intentionally off)
+symbols <- c("IBM", "XOM", "2SG", "TEva", "GOog", "CVX", "AAPL", "BA")
+
+# Location of our database of prices (csv file)
+file_path = "~/Coding/R/Quantitative_Trading_With_R/Chapter2_ToolsOfTheTrade"
+
+# Filter and sort symbols
+filtered_symbols <- filter_symbols(symbols)
+
+# Extract prices
+prices <- extract_prices(filtered_symbols, file_path)
+
+# Filter prices
+missing_rows <- filter_prices(prices)
+
+# Compute correlations
+correlation_matrix <- compute_pairwise_correlations(prices)
+correlation_matrix
