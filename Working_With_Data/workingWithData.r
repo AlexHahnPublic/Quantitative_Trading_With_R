a6dd84322f2020758f5d1b95860c71949db9bdea
AlexHahnPublic <afh53.cornell.edu>
2016-12-06 15:02:59 -0500

    33% done with chapter 3

--- Working_With_Data/workingWithData.r
+++ Working_With_Data/workingWithData.r
@@ -42,3 +42,34 @@ library(RJSONIO)
 
 # Read the file
 out <- fromJSON(content = "Coding/R/Quantitative_Trading_With_R/Working_With_Data/test.json")
+out
+
+# Look at the structure of the resulting object
+str(out)
+
+# Save the aapl_2 data frame as a .csv file
+write.csv(aapl_2, file = "Coding/R/Quantitative_Trading_With_R/Working_With_Data/aapl_2_testsave.csv")
+
+# Now let's try saving a an rdata object (binary/more native, should be more compressed)
+save(aapl_2, file = "Coding/R/Quantitative_Trading_With_R/Working_With_Data/aapl_2_testsave.rdata")
+
+# Let's retrieve the files back into memory and ensure that they are identical
+aapl_original <- aapl_2
+rm(aapl_2)
+load(file = "Coding/R/Quantitative_Trading_With_R/Working_With_Data/aapl_2_testsave.rdata")
+
+# The identical() command can be used to check whether objects are the same
+identical(aapl_original, aapl_2)
+
+# Excel work:
+# -----------------
+# Since excel and spreadsheets are ubiquitous in the finance industry R of
+# Course has methodology to deal with them
+
+# Load in the XLConnect library
+library(XLConnect)
+
+# Create the workbook object
+book <- loadWorkbook("Coding/R/Quantitative_Trading_With_R/Working_With_Data/strategy.strategy.xlsx")
+
+
